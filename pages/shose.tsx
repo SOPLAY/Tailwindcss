912a378aa86642063f2a4ea2f7ab169a0c09c889
soplay <tkdgns1341@gmail.com>
2022-07-07 02:19:34 +0900

    [ shose ] add image , Fix page title, text

--- pages/shose.tsx
+++ pages/shose.tsx
@@ -1,19 +1,21 @@
 import { NextPage } from 'next';
+import Head from 'next/head';
+import Image from 'next/image';
 import React, { useEffect, useRef, useState } from 'react';
 
 const Section = ({ children }: { children?: JSX.Element }) => (
-  <section className='w-full h-screen bg-yellow-200'>{children}</section>
+  <section className='w-full h-screen bg-[#e8e19b]'>{children}</section>
 );
 
 const Home: NextPage = () => {
   const data = [
-    { title: 'page1', text: '스케치' },
-    { title: 'page2', text: 'color1' },
-    { title: 'page3', text: 'color2' },
-    { title: 'page4', text: 'color3' },
-    { title: 'page5', text: 'color4' },
-    { title: 'page6', text: 'color5' },
-    { title: 'page7', text: 'color6' },
+    { title: 'Step 1.', text: '스케치' },
+    { title: 'Step 2.', text: '1차 색칠' },
+    { title: 'Step 3.', text: '2차 색칠' },
+    { title: 'Step 4.', text: '3차 색칠' },
+    { title: 'Step 5.', text: 'base 색칠' },
+    { title: 'Step 6.', text: '세부 색칠' },
+    { title: 'Step 7.', text: '완성!' },
   ];
 
   const [isActivePage, setIsActivePage] = useState(0);
@@ -42,27 +44,16 @@ const Home: NextPage = () => {
   };
   return (
     <>
-      <div ref={targetPageRef}>
-        <Section>
-          <div>페이지 타이틀</div>
-        </Section>
-        {data.map((v, i) => (
-          <Section>
-            <>
-              <div className='pt-32 pl-10'>
-                <h1 className='mb-5 text-3xl text-slate-700'>{v.title}</h1>
-                <p>{v.text}</p>
-              </div>
-            </>
-          </Section>
-        ))}
-      </div>
-      <ul className='fixed z-10 flex flex-col justify-center h-screen text-xl list-disc right-1 text-slate-500'>
+      <Head>
+        <title>Drawing shoes</title>
+      </Head>
+      {/* nav */}
+      <ul className='fixed z-10 flex flex-col justify-center h-screen text-3xl list-disc right-1 text-slate-500'>
         {Array.from({ length: 8 }, (v, i) => i).map((v, i) => (
           <li
             className={`${
               i === isActivePage && 'text-black'
-            } duration-300 cursor-pointer `}
+            } duration-300 cursor-pointer my-[-5px] `}
             onClick={() => onMovePage(i)}
             key={i}
           >
@@ -70,6 +61,41 @@ const Home: NextPage = () => {
           </li>
         ))}
       </ul>
+      {/* content */}
+      <div ref={targetPageRef}>
+        <Section>
+          <div className='flex items-center justify-center h-screen'>
+            <h1 className='w-5/6 text-4xl font-bold text-center'>
+              How&nbsp;to&nbsp;Drawing shoes
+            </h1>
+          </div>
+        </Section>
+        {data.map((v, i) => (
+          <Section key={i}>
+            <>
+              <div
+                className={`fixed bottom-12 right-12 max-w-[750px] md:w-3/5 w-1/2 md:h-1/2 overflow-hidden duration-700 ${
+                  isActivePage !== 0 &&
+                  (isActivePage === i + 1 || isActivePage === i)
+                    ? 'opacity-1'
+                    : 'opacity-0'
+                }`}
+              >
+                <Image
+                  src={`/assets/image/ch2/pic_${i + 1}.jpg`}
+                  width='1000'
+                  height='1000'
+                  className='scale-110 translate-y-1/4 md:-translate-y-1/4 '
+                />
+              </div>
+              <div className='pt-32 pl-10 '>
+                <h1 className='z-10 mb-5 text-3xl text-slate-700'>{v.title}</h1>
+                <p>{v.text}</p>
+              </div>
+            </>
+          </Section>
+        ))}
+      </div>
     </>
   );
 };
