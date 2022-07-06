4fbefe24a8a99f1d81d7d833c50d922808a62191
soplay <tkdgns1341@gmail.com>
2022-07-06 21:40:08 +0900

    [ shose ] Add scroll conter

--- pages/shose.tsx
+++ pages/shose.tsx
@@ -20,22 +20,38 @@ const Home: NextPage = () => {
 
   //useEffect
   useEffect(() => {
-    window?.addEventListener('scroll', function () {});
+    window?.addEventListener('scroll', function () {
+      const scrollY = this.scrollY;
+      const currentPage = Math.floor(
+        (scrollY + window.outerHeight / 2) / window.outerHeight
+      );
+      currentPage !== isActivePage && setIsActivePage(currentPage);
+    });
   });
 
   return (
     <>
       <ul className='fixed right-0 flex flex-col justify-center h-screen text-lg list-disc text-slate-500'>
         {Array.from({ length: 8 }, (v, i) => i).map((v, i) => (
-          <li className={`${i === isActivePage && 'text-black'}`} key={i} />
+          <li
+            className={`${i === isActivePage && 'text-black'} duration-300`}
+            key={i}
+          />
         ))}
       </ul>
       <Section>
         <div>페이지 타이틀</div>
       </Section>
-      <Section>
-        <div>asd</div>
-      </Section>
+      {data.map((v, i) => (
+        <Section>
+          <>
+            <div className='mt-32 ml-10'>
+              <h1 className='mb-5 text-3xl text-slate-700'>{v.title}</h1>
+              <p>{v.text}</p>
+            </div>
+          </>
+        </Section>
+      ))}
     </>
   );
 };
