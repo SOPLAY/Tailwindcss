b57fa7263d0f0be8dc64275af18c0937ddb095e2
soplay <tkdgns1341@gmail.com>
2022-07-07 00:41:29 +0900

    [ shoes ] Fix ul to scroll pages

--- pages/shose.tsx
+++ pages/shose.tsx
@@ -1,5 +1,5 @@
 import { NextPage } from 'next';
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 
 const Section = ({ children }: { children?: JSX.Element }) => (
   <section className='w-full h-screen bg-yellow-200'>{children}</section>
@@ -29,29 +29,45 @@ const Home: NextPage = () => {
     });
   });
 
+  const targetPageRef = useRef<HTMLDivElement>(null);
+
+  const onMovePage = (index: number) => {
+    targetPageRef.current &&
+      window &&
+      window.scrollTo({
+        top: targetPageRef.current.getElementsByTagName('section')[index]
+          .offsetTop,
+        behavior: 'smooth',
+      });
+  };
   return (
     <>
-      <ul className='fixed right-0 flex flex-col justify-center h-screen text-lg list-disc text-slate-500'>
+      <ul className='fixed flex flex-col justify-center h-screen text-xl list-disc right-3 text-slate-500'>
         {Array.from({ length: 8 }, (v, i) => i).map((v, i) => (
           <li
-            className={`${i === isActivePage && 'text-black'} duration-300`}
+            className={`${
+              i === isActivePage && 'text-black'
+            } duration-300 cursor-pointer`}
+            onClick={() => onMovePage(i)}
             key={i}
           />
         ))}
       </ul>
-      <Section>
-        <div>페이지 타이틀</div>
-      </Section>
-      {data.map((v, i) => (
+      <div ref={targetPageRef}>
         <Section>
-          <>
-            <div className='mt-32 ml-10'>
-              <h1 className='mb-5 text-3xl text-slate-700'>{v.title}</h1>
-              <p>{v.text}</p>
-            </div>
-          </>
+          <div>페이지 타이틀</div>
         </Section>
-      ))}
+        {data.map((v, i) => (
+          <Section>
+            <>
+              <div className='pt-32 pl-10'>
+                <h1 className='mb-5 text-3xl text-slate-700'>{v.title}</h1>
+                <p>{v.text}</p>
+              </div>
+            </>
+          </Section>
+        ))}
+      </div>
     </>
   );
 };
