58edf0dc5b5015bbae1a12f453fe9116802f0c28
soplay <tkdgns1341@gmail.com>
2022-07-21 20:38:02 +0900

    [ Card ] Add card setting motion event

--- pages/card.tsx
+++ pages/card.tsx
@@ -1,17 +1,29 @@
+import gsap, { Power3 } from 'gsap';
 import { NextPage } from 'next';
 import Head from 'next/head';
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 
 type CardProps = {
   children: React.ReactNode;
   bgGradient: string;
 };
+
 const Card: React.FC<CardProps> = ({ children, bgGradient }) => (
   <div
-    className={`w-52 h-28 rounded-2xl p-3 bg-white shadow-xl bg-gradient-to-tl ${bgGradient}`}
+    className={`absolute w-[200px] h-28 rounded-2xl p-3 bg-white shadow-[5px_5px_12px_rgba(0,0,0,0.3)] bg-gradient-to-tl ${bgGradient}`}
   >
     <p className={`text-right pr-3 text-white font-bold`}>{children}</p>
   </div>
+);
+
+type ButtonProps = { children: React.ReactNode; onClick: () => void };
+const Button: React.FC<ButtonProps> = ({ children, onClick }) => (
+  <button
+    className='px-3 py-1 m-2 text-white rounded-md bg-black/80'
+    onClick={() => onClick()}
+  >
+    {children}
+  </button>
 );
 
 const Home: NextPage = () => {
@@ -22,20 +34,59 @@ const Home: NextPage = () => {
     { title: 'card', bgGradient: 'from-[#fd1d1d] to-[#833ab4]' },
     { title: 'card', bgGradient: 'from-[#000000] to-[#FC00FF]' },
   ];
+
+  const sectionRef = useRef<HTMLElement>(null);
+  const [windowSize, setWindowSize] = useState({
+    height: 0,
+    width: 0,
+  });
+  const cardSetting = () => {
+    if (sectionRef.current) {
+      const cards = sectionRef.current.getElementsByTagName('div');
+      for (let i = 0; i < cards.length; i++) {
+        gsap.to(cards[i], 0.7, {
+          top: windowSize.height / 2 - i * 40,
+          left: windowSize.width / 2 - 200 + i * 40,
+          rotation: 0,
+          ease: Power3.easeInOut,
+          delay: i * 0.1,
+        });
+      }
+    }
+  };
+  useEffect(() => {
+    if (window) {
+      setWindowSize({ height: window.innerHeight, width: window.innerWidth });
+      window.addEventListener('resize', () => {
+        setWindowSize({ height: window.innerHeight, width: window.innerWidth });
+      });
+    }
+  }, []);
+
+  useEffect(() => {
+    cardSetting();
+  }, [windowSize]);
+
+  const onRandom = () => {};
+
   return (
     <>
       <Head>
         <title>Card-Motion</title>
       </Head>
-      <div className='h-screen bg-purple-100'>
+      <div className='relative h-screen bg-purple-100'>
         <h1 className='my-5 text-3xl text-center font-DoHyeon'>Card-Motion</h1>
-        <section className='flex flex-wrap justify-around'>
+        <section className='flex flex-wrap justify-around' ref={sectionRef}>
           {cardData.map((v, i) => (
             <Card bgGradient={v.bgGradient} key={i}>
               {`${v.title.toUpperCase()} ${i + 1}`}
             </Card>
           ))}
         </section>
+        <div className='fixed w-full text-center bottom-[2%] '>
+          <Button onClick={() => onRandom()}>Random</Button>
+          <Button onClick={() => cardSetting()}>Reset</Button>
+        </div>
       </div>
     </>
   );
